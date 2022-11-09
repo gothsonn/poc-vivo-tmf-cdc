c95bfeed2742cb8c0685c8789aa96d8e59163f2b
Rafael Freitas <gothsonn@gmail.com>
2022-11-08 21:27:16 -0300

    Base para POC de api tmf padrao 666

---
+++ src/main/java/com/accenture/PocVivoTmfCdc.java
@@ -0,0 +1,50 @@
+package com.accenture;
+
+import com.accenture.configuration.LocalDateConverter;
+import com.accenture.configuration.LocalDateTimeConverter;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ExitCodeGenerator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackages = { "com.accenture", "com.accenture.api" , "com.accenture.configuration"})
+public class PocVivoTmfCdc implements CommandLineRunner {
+
+    @Override
+    public void run(String... arg0) throws Exception {
+        if (arg0.length > 0 && arg0[0].equals("exitcode")) {
+            throw new ExitException();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new SpringApplication(PocVivoTmfCdc.class).run(args);
+    }
+
+    @Configuration
+    static class MyConfig extends WebMvcConfigurerAdapter {
+        @Override
+        public void addFormatters(FormatterRegistry registry) {
+            registry.addConverter(new LocalDateConverter("yyyy-MM-dd"));
+            registry.addConverter(new LocalDateTimeConverter("yyyy-MM-dd'T'HH:mm:ss.SSS"));
+        }
+    }
+
+    class ExitException extends RuntimeException implements ExitCodeGenerator {
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public int getExitCode() {
+            return 10;
+        }
+
+    }
+}
